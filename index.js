64a9331f25c7c130b32815472e2a3373a85b83f6
Sajalhowlader <sajolhowlader08@gmail.com>
2022-04-24 15:21:15 +0600

    first commit

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const express = require('express');
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("ME Jhon ready where are you ama")
+})
+
+app.listen(port, () => {
+    console.log('my ema-jhon server is running ', port)
+})
